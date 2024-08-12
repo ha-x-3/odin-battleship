2ad305bf8b4207f01a06401ec059634128273e30
Brittney Epperson <beppers87@hotmail.com>
2024-08-12 17:14:33 -0500

    Added test 2

--- ship.test.js
+++ ship.test.js
@@ -5,3 +5,7 @@ let newShip = new Ship(2);
 it("Should create a new ship object", () => {
     expect(typeof newShip).toBe('object');
 });
+
+it("Should expect the correct length", () => {
+    expect(newShip.length).toBe(2);
+});
