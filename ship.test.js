6fca503092200a69aa42b4d815002e8bb5a1906c
Brittney Epperson <beppers87@hotmail.com>
2024-08-12 17:29:30 -0500

    Added function tests

--- ship.test.js
+++ ship.test.js
@@ -1,6 +1,6 @@
 import Ship from './ship.js';
 
-let newShip = new Ship(2);
+let newShip = Ship(2);
 
 it("Should create a new ship object", () => {
     expect(typeof newShip).toBe('object');
@@ -8,4 +8,35 @@ it("Should create a new ship object", () => {
 
 it("Should expect the correct length", () => {
     expect(newShip.length).toBe(2);
+});
+
+it('Should expect initial sunk status to be false', () => {
+	expect(newShip.sunk).toBe(false);
+});
+
+it('Should expect initial number of hits to be set to 0', () => {
+	expect(newShip.hits).toBe(0);
+});
+
+it('Should sink the ship when hits equal to length', () => {
+	let shipTestSink = Ship(2);
+	shipTestSink.hit();
+	shipTestSink.hit();
+	let sunkenStatus = shipTestSink.isSunk();
+	expect(sunkenStatus).toBe(true);
+});
+
+it('Should not sink the ship when hits lower than length', () => {
+	let shipTestNotSink = Ship(2, 0);
+	shipTestNotSink.hit();
+	let notSunkenStatus = shipTestNotSink.isSunk();
+	expect(notSunkenStatus).toBe(false);
+});
+
+it('Should not allow hits higher than length', () => {
+	let shipHitTest = Ship(2, 0);
+	shipHitTest.hit();
+	shipHitTest.hit();
+	shipHitTest.hit();
+	expect(shipHitTest.hits).toBe(2);
 });
