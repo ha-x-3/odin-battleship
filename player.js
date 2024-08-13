cc27a6c8b3c2574bb5d7c7462568e56e187f7170
Brittney Epperson <beppers87@hotmail.com>
2024-08-13 16:23:11 -0500

    Added Player factory and tests

---
+++ player.js
@@ -0,0 +1,29 @@
+import Gameboard from "./gameboard.js";
+
+export default function Player(name) {
+	const playerBoard = Gameboard();
+
+	const placePlayerShip = (newL, newX, newY, newD) => {
+		playerBoard.placeShip(newL, newX, newY, newD);
+		let placedShips = playerBoard.shipsArray.filter(
+			(ship) => ship.coordPairs.length > 0
+		);
+		placedShips.forEach((ship) => {
+			// document.getElementById(ship.coordPairs.length).style.backgroundColor = "#70717c";
+			//console.log('here');
+		});
+
+		//console.log(playerBoard.shipsArray);
+	};
+
+	const playerShot = (myTarget, myX, myY) => {
+		myTarget.playerBoard.receiveAttack(myX, myY);
+	};
+
+	return {
+		name,
+		playerBoard,
+		placePlayerShip,
+		playerShot,
+	};
+}
--- player.test.js
+++ player.test.js
@@ -1,3 +1,31 @@
-it.skip("Test 1", () => {
-    expect(1).toBe(1);
+import Player from "./player.js";
+import AI from "./AI.js";
+
+let computerPlayer = AI('Computer');
+let player = Player('Enemy');
+
+it('Should create a new player object', () => {
+	expect(player).toBeInstanceOf(Object);
+});
+
+it('Should be able to place a ship on its own board', () => {
+	player.placePlayerShip(1, 0, 0, 'H');
+	expect(player.playerBoard.board).toEqual([
+		['H', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+		['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+	]);
+});
+
+it('Should be able to throw a precise attack on the enemy board', () => {
+	player.playerShot(computerPlayer, 0, 5);
+	expect(computerPlayer.playerBoard.board[0][5]).not.toEqual('o');
+	// may be missed hit or ship hit
 });
