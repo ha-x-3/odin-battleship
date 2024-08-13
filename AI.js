9ae4ec357fd1ae9b3cd0d98077d2b675f49eea82
Brittney Epperson <beppers87@hotmail.com>
2024-08-13 16:09:25 -0500

    Added AI factory and tests

---
+++ AI.js
@@ -0,0 +1,54 @@
+import Gameboard from "./gameboard.js";
+
+export default function AI(name) {
+	const playerBoard = Gameboard();
+	const AIPastShots = [];
+
+	const getRandomX = () => Math.floor(Math.random() * 10);
+	const getRandomY = () => Math.floor(Math.random() * 10);
+	const getRandomDirection = () => (Math.random() < 0.5 ? 'V' : 'H');
+
+	const placeRandomShips = () => {
+		let targetArray = playerBoard.shipsArray;
+		targetArray.forEach((currentShip) => {
+			let isShipPlaced = currentShip.coordPairs.length;
+			while (isShipPlaced === 0) {
+				let randomDirection = getRandomDirection();
+				let ranX = getRandomX();
+				let ranY = getRandomY();
+				let pushLength = currentShip.ship.length;
+				playerBoard.placeShip(pushLength, ranX, ranY, randomDirection);
+				isShipPlaced = currentShip.coordPairs.length;
+			}
+		});
+	};
+
+	const computerShot = (myTarget) => {
+		let shotExists = false;
+		let hitX = getRandomX();
+		let hitY = getRandomY();
+		let pairOfShot = [hitX, hitY];
+		if (AIPastShots.some((shot) => shot[0] === hitX && shot[1] === hitY)) {
+			shotExists = true;
+			while (shotExists) {
+				hitX = getRandomX();
+				hitY = getRandomY();
+				pairOfShot = [hitX, hitY];
+				shotExists = AIPastShots.some(
+					(shot) => shot[0] === hitX && shot[1] === hitY
+				);
+			}
+		}
+		myTarget.playerBoard.receiveAttack(hitX, hitY);
+		AIPastShots.push(pairOfShot);
+	};
+
+	placeRandomShips();
+
+	return {
+		name,
+		playerBoard,
+		AIPastShots,
+		computerShot,
+	};
+}
---
+++ AI.test.js
@@ -0,0 +1,44 @@
+import Player from "./player.js";
+import AI from "./AI.js";
+
+let computerPlayer = AI('Computer');
+let player = Player('Enemy');
+
+it('Should create a new AI object', () => {
+	expect(computerPlayer).toBeInstanceOf(Object);
+});
+
+it('Should expect a pre-filled board with 5 ships', () => {
+	let AIBoard = computerPlayer.playerBoard.shipsArray;
+	let checkBoard = AIBoard.every((ship) => ship.coordPairs.length > 0);
+	expect(checkBoard).toBeTruthy();
+});
+
+it('Should be able to throw a random attack on the enemy board', () => {
+	computerPlayer.computerShot(player);
+	let checkMissedHits = player.playerBoard.hitCoords.missedHits;
+	expect(checkMissedHits.length).toEqual(1);
+	// always a miss because player board is empty
+});
+
+it('Should be able to win against an enemy within maximum 242 shots (11 x 11, twice)', () => {
+	// placing a random ship
+	player.placePlayerShip(5, 0, 0, 'H');
+	let isPlayerSunk = player.playerBoard.allSunk;
+	// continue shooting until player is sunk
+	while (!isPlayerSunk) {
+		computerPlayer.computerShot(player);
+		isPlayerSunk = player.playerBoard.allSunk;
+	}
+	let counter = computerPlayer.AIPastShots.length;
+	expect(isPlayerSunk).toBeTruthy();
+	expect(counter).toBeLessThanOrEqual(242);
+});
+
+it('Should never hit in the same place', () => {
+	let shots = computerPlayer.AIPastShots;
+	function hasDuplicates(array) {
+		return new Set(array.map(JSON.stringify)).size !== array.length;
+	}
+	expect(hasDuplicates(shots)).toBeFalsy();
+});
